b517762f6a4c1b024d4a6d10c8150de1205fa690
mvila <mvila@voxelgroup.net>
2022-01-06 18:17:27 +0100

    refactor: manual inline for total parameter, made plus method non static and moved it to Money class

--- csharp/Hole3/Money.cs
+++ csharp/Hole3/Money.cs
@@ -12,5 +12,15 @@ namespace Hole3
             this.value = value;
             this.currency = currency;
         }
+
+        public Money Plus(Money next)
+        {
+            if (!next.currency.Equals(currency))
+            {
+                throw new Incalculable();
+            }
+
+            return new Money(value + next.value, next.currency);
+        }
     }
 }
